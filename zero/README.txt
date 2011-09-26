3458f2ea9964d7b4d5e0af83d9cc18c2c96734dc
Gaute Hope <eg@gaute.vetsj.com>
2011-09-26 16:26:14 +0200

    del gps.py, update readme

---
+++ zero/README.txt
@@ -0,0 +1,5 @@
+
+ * Upload zeronode.py to node connected to computer
+
+ * Run zero.py to manage and collect data relayed from ZeroNode
+
--- zero/gps.py
+++ zero/gps.py
@@ -14,3 +14,9 @@ class Gps ():
 
   def __init__ (self, b):
     self.buoy = b
+
+  def gps_status (self):
+    print "GPS time: ", self.gps_time, " (T rec: ", self.telegramsreceived, ")"
+    print "Latitude:  ", self.latitude, ('N' if self.north else 'S')
+    print "Longitude: ", self.longitude, ('E' if self.east else 'W') 
+    print "Valid: ", self.valid
