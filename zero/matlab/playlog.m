8ad16e8d0f92f2aad24d40ff93afdcc25f5f6cc2
Gaute Hope <eg@gaute.vetsj.com>
2011-09-28 22:42:36 +0200

    Matlab playback log function

---
+++ zero/matlab/playlog.m
@@ -0,0 +1,45 @@
+function [ny, SFs, y, Fs] = playlog (t, v, nf)
+% PLAYLOG   Playback log file
+%
+% Author: Gaute Hope <eg@gaute.vetsj.com> / 2011-09-28
+%
+% [ny, SFs, y, Fs] = playlog (t, v)
+%
+% t  = time stamps
+% v  = data values
+% nf = speed up playback nf times
+% 
+% Returns:
+% ny = values normalized and centered around 2^23 interpolated
+%      to standard frequency 8192.
+%
+% SFs = standard frequency 8192.
+% y   = Normalized and centered data at original frequency
+% Fs  = Original average frequency
+%
+
+if ~exist('nf', 'var')
+  nf = 1;
+end
+
+%% Normalize the values vector
+% 24 bit
+y = v - 2^23;
+y = y / max(abs(y));
+%%
+
+%% Find avarage frequency, t is in microseconds
+Fs = 10^6 / mean(diff(t));
+
+%% Interpolate to convert to desired frequency
+SFs = 8192;
+
+nt = linspace(t(1), t(end), length(t)*round(SFs/(nf*Fs)));
+ny = interp1(t, y, nt);
+
+%% Playback
+fprintf ('Playing: %6.2f seconds (at %dx speed)..', length(t)/(nf*Fs), nf)
+
+sound (ny, SFs);
+
+end
