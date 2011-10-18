a7689d07904f24222e24889f1aa60ab8d96090ad
Gaute Hope <eg@gaute.vetsj.com>
2011-10-18 21:02:29 +0200

    storereader reading index

--- buoy/store.h
+++ buoy/store.h
@@ -57,17 +57,17 @@ void sd_write_reference (ulong);
 # endif /* ONLY_SPEC */
 
 /* Data format */
-# define STORE_VERSION 1
-# define SAMPLE_LENGTH 3
-# define TIMESTAMP_LENGTH 4
+# define STORE_VERSION 1uL
+# define SAMPLE_LENGTH 3uL
+# define TIMESTAMP_LENGTH 4uL
 
 /* Maximum number of timestamp, sample pairs for each datafile */
-# define EST_MINUTES_PER_DATAFILE 1L
-# define MAX_SAMPLES_PER_FILE (EST_SAMPLE_RATE * 60L * EST_MINUTES_PER_DATAFILE)
+# define EST_MINUTES_PER_DATAFILE 1uL
+# define MAX_SAMPLES_PER_FILE (EST_SAMPLE_RATE * 60uL * EST_MINUTES_PER_DATAFILE)
 # define MAX_REFERENCES (MAX_SAMPLES_PER_FILE / ( EST_SAMPLE_RATE * ROLL_REFERENCE))
 
-# define _SD_DATA_FILE_SIZE (MAX_SAMPLES_PER_FILE * (SAMPLE_LENGTH + TIMESTAMP_LENGTH) + MAX_REFERENCES * 50)
-# define SD_DATA_FILE_SIZE (_SD_DATA_FILE_SIZE + (_SD_DATA_FILE_SIZE % 512))
+# define _SD_DATA_FILE_SIZE (MAX_SAMPLES_PER_FILE * (SAMPLE_LENGTH + TIMESTAMP_LENGTH) + MAX_REFERENCES * 50uL)
+# define SD_DATA_FILE_SIZE (_SD_DATA_FILE_SIZE + (_SD_DATA_FILE_SIZE % 512uL))
 
 /* Data file format {{{
  *
@@ -86,26 +86,26 @@ void sd_write_reference (ulong);
  *
  * }}} */
 
-# define SD_REFERENCE_LENGTH 54
+# define SD_REFERENCE_LENGTH 54uL
 
 /* Last ID is one unsigned long */
 typedef ulong LASTID;
 
 typedef struct _Index {
-  uint version;     // Version of data (as defined in STORE_VERSION)
-  ulong id;         // Id of index (limited by MAXID)
+  uint16_t version;     // Version of data (as defined in STORE_VERSION)
+  uint32_t id;          // Id of index (limited by MAXID)
 
-  uint sample_l;    // Length of sample (bytes)
-  uint timestamp_l; // Length of time stamp (bytes)
+  uint16_t sample_l;    // Length of sample (bytes)
+  uint16_t timestamp_l; // Length of time stamp (bytes)
 
-  ulong samples;    // Can maximum reach MAX_SAMPLES_PER_FILE
-  ulong nrefs;      // Current number of references
-  ulong refs[MAX_REFERENCES]; // List with position of reference points.
+  uint32_t samples;     // Can maximum reach MAX_SAMPLES_PER_FILE
+  uint32_t nrefs;       // Current number of references
+  uint32_t refs[MAX_REFERENCES]; // List with position of reference points.
 } Index;
 
 
 /* Using 8.3 file names limits the ID */
-# define MAXID (10^8 -1)
+# define MAXID (10^8uL -1uL)
 
 /* Files:
  * LASTID.LON     - file with current index id (not to be trusted..)
--- zero/storereader/main.cpp
+++ zero/storereader/main.cpp
@@ -5,22 +5,65 @@
  *
  */
 
-# define byte char
-typedef unsigned long ulong;
-typedef unsigned int  uint;
-
-# define ONLY_SPEC
-# include "store.h"
-
+# include <stdint.h>
+# include <fstream>
 # include <iostream>
+# include <stdlib.h>
 
 using namespace std;
+
+# define ONLY_SPEC
+typedef char byte;
+# include "store.h"
+
+Index open_index (string);
+void  print_index (Index);
 
 int main (int argc, char **argv) {
 
   cerr << "Store reader for Gautebøye ( rev " << VERSION << ")" << endl;
-  cerr << "Store version: " << STORE_VERSION << endl;
+  cerr << "Store version: " << STORE_VERSION << endl << endl;
+
+  string indexfn = "1.IND";
+  string datafn  = "1.DAT";
+
+  Index i = open_index (indexfn);
+  print_index (i);
 
   return 0;
+}
+
+Index open_index (string fn) {
+  cerr << "Opening index " << fn << "..";
+
+  Index i;
+
+  ifstream fi (fn.c_str (), ios::binary);
+
+  if (!fi.is_open ()) {
+    cerr << endl << "[ERROR] Could not open index, exiting..";
+    exit(1);
+  }
+
+  /* Reading index, member by member to avoid struct padding issues */
+  fi.read (reinterpret_cast<char*>(&i.version), sizeof(i.version));
+  fi.read (reinterpret_cast<char*>(&i.id), sizeof(i.id));
+  fi.read (reinterpret_cast<char*>(&i.sample_l), sizeof(i.sample_l));
+  fi.read (reinterpret_cast<char*>(&i.timestamp_l), sizeof(i.timestamp_l));
+  fi.read (reinterpret_cast<char*>(&i.samples), sizeof(i.samples));
+  fi.read (reinterpret_cast<char*>(&i.nrefs), sizeof(i.nrefs));
+  fi.read (reinterpret_cast<char*>(&i.refs), i.nrefs * sizeof(uint32_t));
+
+  cerr << "done." << endl;
+  return i;
+}
+
+void print_index (Index i) {
+  cerr << "=> Index:             " << i.id << endl;
+  cerr << "=> Version:           " << i.version << endl;
+  cerr << "=> Sample length:     " << i.sample_l << endl;
+  cerr << "=> Timestamp length:  " << i.timestamp_l << endl;
+  cerr << "=> Samples:           " << i.samples << endl;
+  cerr << "=> References:        " << i.nrefs << endl;
 }
 
