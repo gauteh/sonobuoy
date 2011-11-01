3157f9e0d3f18044a1aefdd5f528a0b305c66385
Gaute Hope <eg@gaute.vetsj.com>
2011-11-01 11:04:52 +0100

    clean rf200 and coupling

--- buoy/snap/rf200.py
+++ buoy/snap/rf200.py
@@ -27,54 +27,6 @@ def startup ():
   # 2Mbps
   setRadioRate(3)
 
-#  a_setup ()
-
 def testecho ():
   print "Hello there."
-
-#@setHook (HOOK_STDIN)
-def stdinHandler (buf):
-  a_stdinhandler (buf)
-
-waitrpc = False
-doperf  = False
-
-packs   = 100
-remain  = 0
-
-@setHook (HOOK_RPC_SENT)
-def rpc_sent (bufid):
-  global waitrpc
-  waitrpc = False
-
-@setHook (HOOK_1MS)
-def onems (tick):
-  global waitrpc
-  global s
-  global doperf, remain 
-  if doperf:
-    if not waitrpc:
-      waitrpc = rpc (PORTAL_ADDR, 'dobytes', s)
-      remain -= 1
-      if remain < 1: doperf = False
-
-def startperf ():
-  global doperf, packs, remain
-  doperf = True
-  remain = packs 
-
-def whileperf():
-  global remain, waitrpc, s
-  while remain > 1:
-    while waitrpc: pass
-    waitrpc = rpc (PORTAL_ADDR, 'dobytes', s)
-    remain -= 1
-
-def stopperf ():
-  global doperf
-  doperf = False
-
-def setpacks (p):
-  global packs
-  packs = p
 
