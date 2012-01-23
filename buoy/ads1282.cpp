a353ed592013152a5f9cf4308c25f5e25ff4816e
Gaute Hope <eg@gaute.vetsj.com>
2012-01-23 18:02:53 +0100

    i2c alive

--- buoy/ads1282.cpp
+++ buoy/ads1282.cpp
@@ -25,13 +25,15 @@ namespace Buoy {
     drdy        = false;
     value       = 0;
 
-
     return;
   }
 
   //HardwareSPI spi (1);
 
   void ADS1282::setup () {
+
+    delay(5000);
+
     /* Setup AD and get ready for data */
 # if DIRECT_SERIAL
     SerialUSB.println ("[AD] Setting up ADS1282..");
@@ -44,9 +46,8 @@ namespace Buoy {
     pinMode (AD_nDRDY, INPUT);
     pinMode (AD_SS, OUTPUT);
 
-    ADS1282::spi.begin (SPI_1_125MHZ, MSBFIRST, SPI_MODE_0);
+    spi.begin (SPI_1_125MHZ, MSBFIRST, SPI_MODE_0);
     digitalWrite (AD_SS, LOW);
-
 
     /* Configure AD */
     configure ();
@@ -64,7 +65,114 @@ namespace Buoy {
 # if DIRECT_SERIAL
     SerialUSB.println ("[AD] Configuring ADS1282..");
 # endif
-    /* Not implemented */
+
+    int n = 0;
+
+    /* Configure I2C (U7) */
+
+    Wire.beginTransmission (AD_I2C_ADDRESS_W);
+    Wire.send (0x06);
+    Wire.send (AD_I2C_CONTROL1);
+    Wire.send (AD_I2C_CONTROL2);
+    n = Wire.endTransmission ();
+
+    if (n == SUCCESS) SerialUSB.println ("SUCCESS");
+    else {
+      SerialUSB.print ("ERROR: ");
+      SerialUSB.println (n);
+    }
+
+    /* Set up outputs:
+     * - Turn off SYNC
+     * - Turn on  PMODE
+     * - Turn off RESET
+     */
+    Wire.beginTransmission (AD_I2C_ADDRESS_W);
+    Wire.send (0x02);
+    Wire.send (0);
+    Wire.send (AD_I2C_PMODE);
+    n = Wire.endTransmission ();
+    if (n == SUCCESS) SerialUSB.println ("SUCCESS");
+    else {
+      SerialUSB.print ("ERROR: ");
+      SerialUSB.println (n);
+    }
+
+
+    // Read output
+    Wire.beginTransmission (AD_I2C_ADDRESS_R);
+    n = Wire.requestFrom (AD_I2C_ADDRESS_R, 2);
+    SerialUSB.println (n);
+    while (Wire.available ()) {
+
+      uint8 r = Wire.receive ();
+
+      SerialUSB.print  ("[AD] [I2C] Got:");
+      SerialUSB.println (r, BIN);
+    }
+
+    n = Wire.endTransmission ();
+    if (n == SUCCESS) SerialUSB.println ("SUCCESS");
+    else {
+      SerialUSB.print ("ERROR: ");
+      SerialUSB.println (n);
+    }
+
+    // Read input
+    Wire.beginTransmission (AD_I2C_ADDRESS_W);
+    Wire.send (0x00);
+    n = Wire.endTransmission ();
+    if (n == SUCCESS) SerialUSB.println ("SUCCESS");
+    else {
+      SerialUSB.print ("ERROR: ");
+      SerialUSB.println (n);
+    }
+
+    Wire.beginTransmission (AD_I2C_ADDRESS_R);
+    n = Wire.requestFrom (AD_I2C_ADDRESS_R, 2);
+    SerialUSB.println (n);
+    while (Wire.available ()) {
+
+      uint8 r = Wire.receive ();
+
+      SerialUSB.print  ("[AD] [I2C] Got:");
+      SerialUSB.println (r, BIN);
+    }
+
+    n = Wire.endTransmission ();
+    if (n == SUCCESS) SerialUSB.println ("SUCCESS");
+    else {
+      SerialUSB.print ("ERROR: ");
+      SerialUSB.println (n);
+    }
+
+    // Read configuration
+    Wire.beginTransmission (AD_I2C_ADDRESS_W);
+    Wire.send (0x06);
+    n = Wire.endTransmission ();
+    if (n == SUCCESS) SerialUSB.println ("SUCCESS");
+    else {
+      SerialUSB.print ("ERROR: ");
+      SerialUSB.println (n);
+    }
+
+    Wire.beginTransmission (AD_I2C_ADDRESS_R);
+    n = Wire.requestFrom (AD_I2C_ADDRESS_R, 2);
+    SerialUSB.println (n);
+    while (Wire.available ()) {
+
+      uint8 r = Wire.receive ();
+
+      SerialUSB.print  ("[AD] [I2C] Got:");
+      SerialUSB.println (r, BIN);
+    }
+
+    n = Wire.endTransmission ();
+    if (n == SUCCESS) SerialUSB.println ("SUCCESS");
+    else {
+      SerialUSB.print ("ERROR: ");
+      SerialUSB.println (n);
+    }
   }
 
   void ADS1282::read_control_register () {
@@ -84,7 +192,7 @@ namespace Buoy {
     if (!spi_is_rx_nonempty (SPI1)) {
       SerialUSB.println ("[AD] SPI is empty.");
     } else {
-      byte a = ADS1282::spi.read ();
+      byte a = spi.read ();
       SerialUSB.print ("[AD] Got value: ");
       SerialUSB.println (a);
     }
--- buoy/ads1282.h
+++ buoy/ads1282.h
@@ -14,15 +14,35 @@ namespace Buoy {
   /* I2C bus
    *
    * The I2C control unit (U7 on ADS1282-EVM schematic) appears to be an
-   * PCA9535RGE, address for read and write is obtained from PCA9536
-   * datasheet.
+   * PCA9535RGE.
    */
 
 # define AD_I2C  1
 # define AD_SCL 38
 # define AD_SDA 39
-# define AD_I2C_ADDRESS_R 0b10000011 // For read operations
-# define AD_I2C_ADDRESS_W 0b10000010 // For write operations
+# define AD_I2C_ADDRESS_R 0x20 // For read operations
+# define AD_I2C_ADDRESS_W 0x20 // For write operations
+
+/* Control register of PCA953RGE:
+ * HIGH is input
+ * LOW  is output
+ *
+ * default is output.
+ */
+
+/* Inputs, register 1 */
+# define AD_I2C_MFLAG 0b10000000
+
+/* Outputs, register 1 */
+# define AD_I2C_SYNC  0b01000000
+
+/* Register 2 */
+# define AD_I2C_PMODE 0b00000001
+# define AD_I2C_RESET 0b00000010
+
+# define AD_I2C_CONTROL1 AD_I2C_MFLAG
+# define AD_I2C_CONTROL2 0
+
 
   /* SPI */
 # define AD_SPI  1
