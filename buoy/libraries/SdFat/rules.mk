644bf8dabdf2fec8c296ba27d57dba8100f8e2bd
Gaute Hope <eg@gaute.vetsj.com>
2012-02-05 18:19:16 +0100

    initial sdfat

---
+++ buoy/libraries/SdFat/rules.mk
@@ -0,0 +1,37 @@
+# Standard things
+sp := $(sp).x
+dirstack_$(sp) := $(d)
+d := $(dir)
+BUILDDIRS += $(BUILD_PATH)/$(d)
+
+# Local flags
+CFLAGS_$(d) := $(WIRISH_INCLUDES) $(LIBMAPLE_INCLUDES)
+
+# Local rules and targets
+cSRCS_$(d) :=
+
+cppSRCS_$(d) := istream.cpp \
+								ostream.cpp \
+								Sd2Card.cpp \
+								SdBaseFile.cpp \
+								SdFat.cpp \
+								SdFatUtil.cpp \
+								SdFile.cpp \
+								SdStream.cpp \
+								SdVolume.cpp
+
+cFILES_$(d) := $(cSRCS_$(d):%=$(d)/%)
+cppFILES_$(d) := $(cppSRCS_$(d):%=$(d)/%)
+
+OBJS_$(d) := $(cFILES_$(d):%.c=$(BUILD_PATH)/%.o) \
+                 $(cppFILES_$(d):%.cpp=$(BUILD_PATH)/%.o)
+DEPS_$(d) := $(OBJS_$(d):%.o=%.d)
+
+$(OBJS_$(d)): TGT_CFLAGS := $(CFLAGS_$(d))
+
+TGT_BIN += $(OBJS_$(d))
+
+# Standard things
+-include $(DEPS_$(d))
+d := $(dirstack_$(sp))
+sp := $(basename $(sp))
