b82c915ce8037c4cccb57cd14b198b0b61586205
Gaute Hope <eg@gaute.vetsj.com>
2012-02-28 21:01:57 +0100

    plot fft

---
+++ zero/matlab/frequence_analysis.m
@@ -0,0 +1,27 @@
+function [F, Ft] = frequence_analysis (d)
+% Do fourier transform of data d (signed input), normalize and plot
+% Returns:
+% F  = Frequency scale
+% Ft = Fourier space values
+
+n = length (d);
+N = 2^nextpow2 (n);
+
+Ft = fft (d, N);
+
+freq = 250; % Sample rate
+
+X = abs (fftshift (Ft));
+X = X / norm(X);          % Normalize
+
+F = freq * [ -N/2 : (N/2 -1)] / N;
+
+figure (2);
+clf ('reset');
+plot (F, X);
+title ('Frequency domain');
+xlabel ('Frequency [Hz]');
+ylabel ('Amplitude (normalized)');
+ylim ([-.1 1.1]);
+
+end
