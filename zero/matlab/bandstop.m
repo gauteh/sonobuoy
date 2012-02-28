c62cdb219087527780e4a9d58f4021c2681a77dc
Gaute Hope <eg@gaute.vetsj.com>
2012-02-28 21:02:15 +0100

    WIP: bandstop dft filter

---
+++ zero/matlab/bandstop.m
@@ -0,0 +1,48 @@
+function [passed, blocked] = bandstop (d, low, high)
+% Filter d for frequencies between low and high and return passed and
+% blocked
+
+n = length (d);
+N = 2^nextpow2 (n);
+
+freq = 250; % Sample rate
+
+assert (low >= 0 && low <= (freq /2 ), 'Bad low frequency');
+assert (high >= 0 && high <= (freq / 2), 'Bad high frequency');
+
+Ft = fft (d, N);
+F  = freq * [ - N/2 : (N / 2 - 1) ] / N;
+
+lown  = floor((low / freq) * (N-1));
+highn = ceil((high / freq) * (N-1));
+
+Blocked = zeros (N,1);
+
+Blocked((lown+1):(highn+1)) = Ft((lown+1):(highn+1));
+Blocked(end-highn:end-lown) = Ft(end-highn:end-lown);
+
+Passed = Ft - Blocked;
+
+figure (3);
+clf ('reset');
+subplot (3, 1, 1);
+plot (F, fftshift(abs(Ft)));
+title ('Original');
+xlabel ('Frequency (Hz)');
+
+subplot (3, 1, 2);
+plot (F, fftshift (abs(Passed)));
+title ('Passed');
+xlabel ('Frequency (Hz)');
+
+subplot (3, 1, 3);
+plot (F, fftshift (abs(Blocked)));
+title ('Blocked');
+xlabel ('Frequency (Hz)');
+
+passed = ifft (Passed, N);
+passed = passed(1:n);
+blocked = ifft (Blocked, N);
+blocked = blocked(1:n);
+
+end
