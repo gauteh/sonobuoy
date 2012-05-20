768ef899ef2c4911fd5bf849573c11f5bdae9c58
Gaute Hope <eg@gaute.vetsj.com>
2012-05-21 01:35:59 +0200

    clean up

--- zero/utils/znportalmode.sh
+++ zero/utils/znportalmode.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
+#
+# Sets zeronode (RF200 on USB module) to portal mode where it may be accessed.
+# by Synapses Portal.
 
-echo '$ZP*NN' > /dev/ttyUSB1
+# Default
+usb=/dev/ttyUSB0
+if [ $# -gt 0 ]; then
+  usb=$1
+fi
+
+echo "Enabling Portal mode on node connected to: ${usb}.."
+
+echo '$ZP*NN' > $usb
 
