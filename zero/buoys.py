efcc2c80cfe36ac3ebe36c38b714a3fa4850f8e9
Gaute Hope <eg@gaute.vetsj.com>
2012-05-21 19:48:13 +0200

    new rf dev on buoy one

--- zero/buoys.py
+++ zero/buoys.py
@@ -6,7 +6,8 @@
 
 buoys = [ { 'id' :        1,
             'name' :      'One',
-            'address' :   '03:FF:40',
+            'address' :   '00:00:FF',
+            #'address' :   '03:FF:40',
             'enabled' :   True,
           },
 
