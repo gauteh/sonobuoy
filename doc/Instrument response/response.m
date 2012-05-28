609992abe39c46b9bfcf46a921485067b02a5b9d
Gaute Hope <eg@gaute.vetsj.com>
2012-05-28 20:16:11 +0200

    upd response

--- doc/Instrument response/response.m
+++ doc/Instrument response/response.m
@@ -1,9 +1,9 @@
 % Calculates instrument response
-r1 = 58.33e3;
-r2 = 41.67e3;
+r1 = 58.33e3;  % ohm
+r2 = 41.67e3;  % ohm
 
-c1 = 47e-6;
-c2 = 380e-12;
+c1 = 47e-6;    % farad
+c2 = 380e-12;  % farad
 
 syms s i1 i2 vs;
 zc1 = 1/(s*c1);
@@ -12,12 +12,13 @@ zc2 = 1/(s*c2);
 zr2 = r2;
 
 [i1, i2] = solve ('vs = i1*(zc1 + zr1) + (i1 - i2)*zr2', ...
-           '0  = (i2 - i1)*zr2 + i2*zc2', 'i1', 'i2');
+                  '0  = (i2 - i1)*zr2 + i2*zc2', 'i1', 'i2');
+%i2 = solve ('vs = i2*(zc1 + zr1 + zr2)', 'i2');
 
 vo = i2*sym('zc2');
 H  = vo/vs;
 pretty (simplify(H))
-H  = subs (H);
+H  = simplify(subs (H));
 
 pretty(H);
 
@@ -30,5 +31,25 @@ T = tf(Tn, Td)
 
 P = bodeoptions;
 P.FreqUnits = 'Hz';
-
+figure(1); clf('reset');
 bodeplot (T, P);
+
+if isstable(T)
+  disp ('Stable.');
+else
+  disp ('Unstable.');
+end
+
+zpk(T)
+
+% upper low pass
+U = zpk([], [-(1/(600*10*10^-9))], 1/(600*10*10^-9));
+figure(2); clf('reset');
+bodeplot (U, P);
+title ('Bode Diagram (Upper low pass filter)');
+
+% final response
+C = U * T;
+figure(3); clf('reset');
+bodeplot (C, P);
+title ('Bode Diagram (Complete analog response)');  
