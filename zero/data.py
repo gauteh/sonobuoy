f5acead85f004cbd2e227c32d6a3aabf8649bfb5
Gaute Hope <eg@gaute.vetsj.com>
2012-06-14 10:21:02 +0200

    update stats

--- zero/data.py
+++ zero/data.py
@@ -161,7 +161,7 @@ class Data:
 
   def got_chunk (self, refno, start, length, reference, status, samples):
     if self.enabled:
-      self.logger.info (self.me + " Got chunk ref: " + str(refno) + ", start: " + str(start) + ", length: " + str(length))
+      self.logger.info (self.me + " Got chunk, ref: " + str(refno) + ", start: " + str(start) + ", length: " + str(length))
 
       self.dataf_l.acquire ()
 
