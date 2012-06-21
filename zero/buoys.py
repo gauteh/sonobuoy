69f8037dacb9a07c87917b3f4d677f565ee9dae5
Gaute Hope <eg@gaute.vetsj.com>
2012-06-21 20:48:50 +0200

    addresses of all buoys

--- zero/buoys.py
+++ zero/buoys.py
@@ -5,15 +5,32 @@
 
 buoys = [ { 'id' :        1,
             'name' :      'One',
-            #'address' :   '00:00:FF',
-            'address' :   '03:FF:40',
+            'address' :   '04:3C:60',
             'enabled' :   True,
           },
 
           { 'id' :        2,
             'name' :      'Two',
-            'address' :   '00:00:00',
-            'enabled' :   False,
+            'address' :   '00:00:FF',
+            'enabled' :   True,
+          },
+
+          { 'id' :        3,
+            'name' :      'Three',
+            'address' :   '04:3C:64',
+            'enabled' :   True,
+          },
+
+          { 'id' :        4,
+            'name' :      'Four',
+            'address' :   '04:3C:93',
+            'enabled' :   True,
+          },
+
+          { 'id' :        5,
+            'name' :      'Five',
+            'address' :   '03:FF:40',
+            'enabled' :   True,
           },
         ]
 
