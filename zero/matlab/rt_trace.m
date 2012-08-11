ec200ab1988930eb55d7d40bc09c443cd3c4a69c
Gaute Hope <eg@gaute.vetsj.com>
2012-08-11 16:35:13 +0200

    rt_trace: simplified

--- zero/matlab/rt_trace.m
+++ zero/matlab/rt_trace.m
@@ -1,8 +1,8 @@
-function t = rt_trace(buoy, fig)
+function tim = rt_trace(buoy, fig)
 % rt_trace (buoy, figure): checks for new data and updates plot with trace
 % continuosly for specified buoy. Specifiy which figure to use.
 %
-% Returns the timer which checks for new data, stop this to stop the
+% Returns the timer which checks for new data, stop/delete this to stop the
 % script.
 
 lastid          = 0;
@@ -11,6 +11,13 @@ show_magnitudes = true;
 show_legend     = false;
 seconds_axis    = true;
 figure (fig);
+
+filelength = 40960; % 40 references of 1024 long batches
+partlyread = false;
+newread    = 0;
+
+t = [];
+d = [];
 
 function updateplot_fcn (obj, event)
   % Check for new batch and plot
@@ -36,27 +43,46 @@ function updateplot_fcn (obj, event)
 
   if (n(end) > lastid)
     disp ('New data..');
-
-    [t, d] = readrangedtt (n(end-n_traces+1:end), mpath);
+    
+    newrange = n(end)-4:n(end);
+    
+    % read new
+    [nt, nd] = readrangedtt (newrange, mpath);
+    t = nt;
+    d = nd;
+    
     if seconds_axis
       t = linspace (0, length(t)/250, length(t));
     end
+    
     plotdtt (t, d, show_magnitudes, false, gca(fig), seconds_axis);
     if ~show_legend
       legend off;
     end
 
 
-    if length(t) == (n_traces * 40960)
+    if length(t) >= (n_traces * filelength)
       lastid = n(end);
+      partlyread = false;
+       
+    else
+      partlyread = true;
     end
+    
+    if seconds_axis
+      xlabel ('Seconds');
+    else
+      xlabel ('Time [us]');
+    end
+    title (sprintf('Continuous trace: %s (last id: %d)', buoy, n(end)));
+    ylabel ('Amplitude');
   end
 end
 
-t  = timer ('ExecutionMode', 'fixedDelay', 'BusyMode', 'drop', 'Period', 5.0, 'TimerFcn', @updateplot_fcn, 'StartDelay', 2);
-start (t);
+tim  = timer ('ExecutionMode', 'fixedDelay', 'BusyMode', 'drop', 'Period', 5.0, 'TimerFcn', @updateplot_fcn, 'StartDelay', 2);
+start (tim);
 
-disp ('rt_trace: Timer started, use delete (t) to stop and remove timer.');
+disp ('rt_trace: Timer started, use delete (tim) to stop and remove the returned timer. Or delete(timerfindall()).');
 
 end
 
