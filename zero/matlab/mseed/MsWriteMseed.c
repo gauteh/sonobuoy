24d8b8b5a1dea5c990d0f2256b573ef1f40180f7
Gaute Hope <eg@gaute.vetsj.com>
2012-10-10 10:27:31 +0200

    initial: mswritemseed

---
+++ zero/matlab/mseed/MsWriteMseed.c
@@ -0,0 +1,17 @@
+/* Author:  Gaute Hope <eg@gaute.vetsj.com>
+ * Date:    2012-10-10
+ *
+ * Matlab interface to libmseed for writing miniSEED files.
+ *
+ */
+
+# include <mex.h>
+# include <libmseed/libmseed.h>
+
+void mexFunction (int nhls, mxArray *phls[], int nhrs, const mxArray *prhs[]) {
+
+  mexPrintf ( "MsWriteMseed - Gaute Hope <eg@gaute.vetsj.com> / 2012-10-10\n");
+
+
+}
+
