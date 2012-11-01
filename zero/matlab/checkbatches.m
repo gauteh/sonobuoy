9b3c19c01134f7fe37b200fe3e6c527dc55fd9a4
Gaute Hope <eg@gaute.vetsj.com>
2012-11-01 17:13:56 +0100

    wip: makehourly

---
+++ zero/matlab/checkbatches.m
@@ -0,0 +1,12 @@
+function checkbatches (refs, t, d, fix)
+% Check for errors in batch and optionally try to fix
+%
+% Attempt to detect errors of type:
+% - Clock skew
+% - SD lag
+% - Bad status and position
+
+if ~exist ('fix', 'var'), fix = false; end
+
+
+end
---
+++ zero/matlab/mseed/makehourly.m
@@ -0,0 +1,12 @@
+function makehourly (network, location, station, range)
+% Make hourly miniSEED files from DAT files
+%
+% Objectives:
+% - Detect timing errors and stop
+% - Save track and status along with each hourly miniSEED file
+
+channel = 'BNR'; % default
+
+
+
+end
