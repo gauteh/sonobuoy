684f467eb85f2e7d95b26cc3b7815d150a0de2c7
Gaute Hope <eg@gaute.vetsj.com>
2012-12-23 12:17:44 +0100

    dattomseed.py: start obspy interface to mseed

--- zero/matlab/mseed/utils.m
+++ zero/matlab/mseed/utils.m
@@ -1,9 +1,7 @@
 % utility functions for quickly generating segments
 
 
-% makesegment shortcuts, assumes data has been loaded into variables:
-
-
-gak2 = @(r, t, d, sdlag)( makesegment('G3', 'GAK2', r, t, d, sdlag));
-gak3 = @(r, t, d, sdlag)( makesegment('G3', 'GAK3', r, t, d, sdlag));
-gak4 = @(r, t, d, sdlag)( makesegment('G3', 'GAK4', r, t, d, sdlag));
+% makesegment shortcuts
+gak2 = @(r, t, d, sdlag)( makesegment('G3', 'GAK2', r, t, d, sdlag) );
+gak3 = @(r, t, d, sdlag)( makesegment('G3', 'GAK3', r, t, d, sdlag) );
+gak4 = @(r, t, d, sdlag)( makesegment('G3', 'GAK4', r, t, d, sdlag) );
---
+++ zero/storetools/ob/dattomseed.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python2
+#
+# Author: Gaute Hope <eg@gaute.vetsj.com> / 2012-12-23
+#
+# Use OBSPY to create miniseed files from DATs
+#
+
+import os
+import sys
+
