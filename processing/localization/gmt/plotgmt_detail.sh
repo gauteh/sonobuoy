e3da5f8a67ab9a0e52f0ca06f7ad2d6fad418cd3
Gaute Hope <eg@gaute.vetsj.com>
2013-02-04 13:39:18 +0100

    + plotgmt_detail

---
+++ processing/localization/gmt/plotgmt_detail.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+#
+# Author: Gaute Hope <eg@gaute.vetsj.com> / 2012-11-29
+#
+# Plot event localization on IBCAO bathymetry using GMT
+#
+
+# Set up GMT defaults
+#gmtset PLOT_DEGREE_FORMAT -ddd:mm:ssF
+#gmtset D_FORMAT=%3.0f
+#gmtset ANOT_FONT_SIZE 10p
+
+out=$1
+if [ $# -ne 1 ]; then
+  out=ibcao_det.ps
+fi
+
+echo "Output: $out"
+
+width=800
+height=700
+gmtset PAGE_ORIENTATION=landscape
+gmtset PAPER_MEDIA=Custom_${width}x${height}
+
+# IBCAO grid
+ibcaogrd=~/ymse/maps/IBCAO-3rd-Edition/IBCAO_Ver3_RR_2012-03-16.grd
+data=~/dev/gautebuoy/processing/localization/gmt
+
+
+# boundaries: read from cartesian.asc
+xmin=0
+ymin=-637312.873449
+xmax=127935.668703
+ymax=-477462.415701
+
+xmind=0
+ymind=84.2
+xmaxd=15
+ymaxd=85.2
+
+rm $out
+
+# Create shaded relief
+echo "Create shaded relief.."
+grdimage ${ibcaogrd} -I${data}/gradient.grd -R${xmin}/${ymin}/${xmax}/${ymax}r -JX20/18.78 -C${data}/ibcao.cpt -P -K -V > $out
+
+
+# Add coast and map box
+export HDF5_DISABLE_VERSION_CHECK=1
+pscoast -R${xmind}/${ymind}/${xmaxd}/${ymaxd}r -JS0/90/20 -Ba5g5/a1g1WeSn -Df -W -O -K >> $out
+
+# stations
+psxy -J -R -O stations.d -St2p -Gyellow -K  >> $out
+pstext -J -R -O stations.t -Gblack -K >> $out
+
+# quakes
+psxy -J -R -O quakes.d -Sa3p -Gred -K >> $out
+
+# add color scale
+psscale -D600p/250p/500p/30p -O -C${data}/ibcao.cpt -I -E -B1000:Depth:/:m: -K >> $out
+
+#misc="-O -K -Sa0.2 -W1p/0 -G0"
+#psxy $region $projection $misc << END >> ibcao.ps
+#80.0 80.0
+#END
+
